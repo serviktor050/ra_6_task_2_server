5420042829524a027fff3080c17e8261401f71a2
serviktor050 <mr.s-gagulin@yandex.ru>
2021-03-10 16:38:59 +0300

    Добавлены корректировки в сервер

--- server.js
+++ server.js
@@ -36,4 +36,4 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7777;
 const server = http.createServer(app.callback());
-server.listen(port, () => console.log("server started"));
+server.listen(port, () => console.log("Сервер работает"));
